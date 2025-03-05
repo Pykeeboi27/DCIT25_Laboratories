c4e9a85f7ee24832739e0705f8414a22f09684ae
Jiroh Carpio <jirohreinier.carpio@cvsu.edu.ph>
2025-03-05 18:33:40 +0800

    Made a user-defined LinkedList with the ability to add and delete values from it. As well as printing the LinkedList.

--- src/Lab1_LinkedList/LinkedList.java
+++ src/Lab1_LinkedList/LinkedList.java
@@ -1,8 +1,75 @@
 package Lab1_LinkedList;
 
 class ListNode{
+    int value;
+    ListNode next;
 
+    ListNode(int value){
+        this.value = value;
+        this.next = null;
+    }
 }
 
 public class LinkedList {
+
+    private ListNode head;
+
+    public void add(int value){
+
+        ListNode newNode = new ListNode(value);
+
+        if (head == null){
+            head = newNode;
+            return;
+        }
+
+        else{
+            ListNode currentNode = head;
+
+            while (currentNode.next != null){
+                currentNode = currentNode.next;
+            }
+
+            currentNode.next = newNode;
+        }
+    }
+
+    public void printList(){
+        ListNode currentNode = head;
+
+        while (currentNode != null){
+            System.out.print(currentNode.value + " -> ");
+            currentNode = currentNode.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public void delete(int value){
+
+        if (head == null){
+            System.out.println("No value to delete. List is empty");
+            return;
+        }
+        else if (head.value == value){
+            head = head.next;
+        }
+
+        else{
+            ListNode currentNode = head;
+
+            while (currentNode.next.value != value){
+
+                if (currentNode.next.next == null){
+                    System.out.println("The value " + value + ", is not on the list.");
+                    return;
+                }
+                else{
+                    currentNode = currentNode.next;
+                }
+            }
+            currentNode.next = currentNode.next.next;
+
+        }
+    }
 }
--- src/Lab1_LinkedList/main.java
+++ src/Lab1_LinkedList/main.java
@@ -3,6 +3,22 @@ package Lab1_LinkedList;
 public class main {
     public static void main(String[] args){
 
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.printList();
+
+        linkedList.add(3);
+        linkedList.add(2);
+        linkedList.add(1);
+        linkedList.printList();
+
+        linkedList.delete(3);
+        linkedList.printList();
+        linkedList.add(3);
+
+        linkedList.delete(2);
+        linkedList.printList();
+        linkedList.delete(5);
     }
 }
 
