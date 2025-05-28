c5b6460bf5cd0f83d305c593d3beaba193f5f272
Jiroh Carpio <jirohreinier.carpio@cvsu.edu.ph>
2025-05-28 18:45:50 +0800

    defined new node structure and instantiated a new tree in the main class

---
+++ src/Lab2_BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,18 @@
+package Lab2_BinarySearchTree;
+
+class Node{
+
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+        this.data = data;
+    }
+}
+
+public class BinarySearchTree {
+
+    Node root;
+}
+
