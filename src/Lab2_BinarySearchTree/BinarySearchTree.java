178fd7997e4a85593f7cb2f0b028911e2279ab70
Jiroh Carpio <jirohreinier.carpio@cvsu.edu.ph>
2025-05-28 19:28:44 +0800

    add a delete method to delete any value that the user wants to delete

--- src/Lab2_BinarySearchTree/BinarySearchTree.java
+++ src/Lab2_BinarySearchTree/BinarySearchTree.java
@@ -65,5 +65,61 @@ public class BinarySearchTree {
         }
 
     }
+
+    public void remove(int data){
+        if (search(data)){
+            removeHelper(root, data);
+        }
+        else{
+            System.out.println(data + " could not be found.");
+        }
+    }
+
+    private Node removeHelper(Node root, int data){
+
+        if (root == null){
+            return root;
+        }
+        else if (root.data > data){
+            root.left = removeHelper(root.left, data);
+        }
+        else if (root.data < data){
+            root.right = removeHelper(root.right, data);
+        }
+        else{ //node found
+            if (root.left == null && root.right == null){
+                root = null;
+            }
+            else if (root.right != null){
+                root.data = successor(root);
+                root.right = removeHelper(root.right, root.data);
+            }
+            else{
+                root.data = predeccessor(root);
+                root.left = removeHelper(root.left, root.data);
+            }
+        }
+        return root;
+    }
+
+    private int successor(Node root){
+        root = root.right;
+
+        while(root.left != null){
+            root = root.left;
+        }
+
+        return root.data;
+    }
+    private int predeccessor(Node root){
+        root = root.left;
+
+        while(root.right != null){
+            root = root.right;
+        }
+
+        return root.data;
+    }
+
 }
 
