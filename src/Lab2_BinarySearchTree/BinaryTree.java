63247d177d31171f49400a50f8f3ed168ec36b28
Jiroh Carpio <jirohreinier.carpio@cvsu.edu.ph>
2025-05-28 18:40:53 +0800

    cleaned all code to start from scratch

--- src/Lab2_BinarySearchTree/BinaryTree.java
+++ src/Lab2_BinarySearchTree/BinaryTree.java
@@ -1,44 +1,10 @@
 package Lab2_BinarySearchTree;
 
 class TreeNode{
-    int value;
-    TreeNode left, right;
 
-    TreeNode(int value){
-        this.value = value;
-        this.right = null;
-        this.left = null;
-    }
 }
 
 public class BinaryTree {
-
-    TreeNode root;
-
-    public void add(int value){
-        root = addRecursive(root, value);
-    }
-
-    private TreeNode addRecursive(TreeNode current, int value){
-        if (current == null){
-            System.out.println(value + " added!");
-            return new TreeNode(value);
-        }
-
-        if (current.value == value){
-            System.out.println(value + " is already on the list.");
-            return current;
-        }
-        if (value > current.value){
-            current.left = addRecursive(current.left, value);
-        }
-        else{
-            current.right = addRecursive(current.right, value);
-        }
-        return current;
-    }
-
-
 
 }
 
--- src/Lab2_BinarySearchTree/main.java
+++ src/Lab2_BinarySearchTree/main.java
@@ -4,16 +4,6 @@ public class main {
 
     public static void main(String[] args){
 
-        BinaryTree tree = new BinaryTree();
-
-        tree.add(47);
-        tree.add(48);
-        tree.add(79);
-        tree.add(21);
-        tree.add(10);
-        tree.add(22);
-        tree.add(41);
-        tree.add(48);
 
     }
 }
